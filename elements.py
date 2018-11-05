7a3012564d2c416233ba97d6b7707efdcd4775bf
suddenlykevin <kxie19@choate.edu>
2018-11-05 15:09:44 -0500

    class 2 commit

--- elements.py
+++ elements.py
@@ -53,10 +53,30 @@ class PeriodicTable:
 				if element.name == value:
 					return element.identify()
 	def parse(self,compound):
-		pass
+		compound = list(compound) # https://stackoverflow.com/questions/4978787/how-to-split-a-string-into-array-of-characters
+		totalweight = 0.0
+		for n in range(len(compound)):
+			if compound[n].isupper()==True:
+				multiplier = 1.0
+				symbol = compound[n]
+				try:
+					if compound[n+1].islower()==True:
+						symbol = compound[n]+compound[n+1]
+						if compound[n+2].isalpha()==False:
+							multiplier = float(compound[n+2])
+					else:
+						if compound[n+1].isalpha()==False:
+							multiplier = float(compound[n+1])
+				except IndexError:
+					pass
+				for element in self.element:
+					if element.symbol == symbol:
+						totalweight += float(element.weight)*multiplier
+		return totalweight
+        
 	def __str__(self):
 		return str(self.element)
 	__repr__ = __str__ 
 
 initPeriodicTable=PeriodicTable()
-print(initPeriodicTable.index(input("Please input name/symbol/number\n>>> ")))
+print(initPeriodicTable.parse(input("Please input molecular formula\n>>> ")))

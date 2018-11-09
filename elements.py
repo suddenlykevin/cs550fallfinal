9ba5cb98f50a01e472f64a86fa7157248ec897e5
suddenlykevin <kxie19@choate.edu>
2018-11-09 11:54:57 -0500

    getters and setters

--- elements.py
+++ elements.py
@@ -2,49 +2,56 @@ import csv
 # from gtts import gTTS
 import os
 import time
+import sys
 
 class Element:
-	def __init__(self,*argv):
-		n=0
-		for arg in argv[0]:
-				if n==0:
-					self.name = arg
-				elif n==1:
-					self.number = arg
-				elif n==2:
-					self.symbol = arg
-				elif n==3:
-					self.weight = arg
-				elif n==4:
-					self.boil = arg
-				elif n==5:
-					self.melt = arg
-				elif n==6:
-					self.density = arg
-				elif n==7:
-					self.fusion = arg
-				n+=1
-	def identify(self):
-		# name = "name: "+self.name
-		# number = (" Atomic number: "+self.number)
-		# symbol= ("symbol: "+self.symbol)
-		# weight = ("\n weight: "+self.weight+"grams per mol")
-		# language = 'en'
-		# myobj = gTTS(text=name, lang=language, slow=False) 
-		# myobj.save("name.mp3") 
-		# os.system("afplay name.mp3")
-		# myobj = gTTS(text=number, lang=language, slow=False)
-		# myobj.save("number.mp3") 
-		# os.system("afplay number.mp3") 
-		# myobj = gTTS(text=symbol, lang=language, slow=False)
-		# myobj.save("symbol.mp3") 
-		# os.system("afplay symbol.mp3") 
-		# myobj = gTTS(text=weight, lang=language, slow=False)
-		# myobj.save("weight.mp3") 
-		# os.system("afplay weight.mp3") 
-		return "Name: "+self.name+"\nNumber: "+self.number+"\nSymbol: "+self.symbol+"\nWeight: "+self.weight+ "g/mol"
+	def __init__(self):
+		pass
+	def setName(self,value):
+		self.name = value
+	def setNumber(self,value):
+		self.number = int(value)
+	def setSymbol(self,value):
+		self.symbol = value
+	def setWeight(self,value):
+		self.weight = float(value)
+	def setBoil(self,value):
+		try:
+			self.boil = float(value)
+		except:
+			pass
+	def setMelt(self,value):
+		try:
+			self.melt = float(value)
+		except:
+			pass
+	def setDensity(self,value):
+		try:
+			self.density = float(value)
+		except:
+			pass
+	def setFusion(self,value):
+		try:
+			self.density = float(value)
+		except:
+			pass
+	def getName(self):
+		return self.name
+	def getNumber(self):
+		return self.number
+	def getSymbol(self):
+		return self.symbol
+	def getWeight(self):
+		return self.weight
+	def getBoil(self):
+		return self.boil
+	def getMelt(self):
+		return self.melt
+	def getDensity(self):
+		return self.density
+	def getFusion(self):
+		return self.fusion
 
-		
 	def __str__(self):
 		return str(self.symbol)
 	__repr__ = __str__ 
@@ -53,32 +60,48 @@ class PeriodicTable:
 	def __init__(self):
 		self.element=[]
 		with open('elements.csv') as csv_file:
-			csv_reader = csv.reader(csv_file, delimiter=",")
-			line_count=0
-			for row in csv_reader:
-				if line_count==0:
-					line_count+=1
+			csv_reader = list(csv.reader(csv_file, delimiter=","))
+			for x in range(len(csv_reader)):
+				if x==0:
+					pass
 				else:
-					self.element.append(Element(row))
-					line_count +=1
+					self.element.append(Element())
+					for n in range(len(csv_reader[x])):
+						if n==0:
+							self.element[x-1].setName(csv_reader[x][n])
+						elif n==1:
+							self.element[x-1].setNumber(csv_reader[x][n])
+						elif n==2:
+							self.element[x-1].setSymbol(csv_reader[x][n])
+						elif n==3:
+							self.element[x-1].setWeight(csv_reader[x][n])
+						elif n==4:
+							self.element[x-1].setBoil(csv_reader[x][n])
+						elif n==5:
+							self.element[x-1].setMelt(csv_reader[x][n])
+						elif n==6:
+							self.element[x-1].setDensity(csv_reader[x][n])
+						elif n==7:
+							self.element[x-1].setFusion(csv_reader[x][n])
 	def index(self,value):
 		try:
-			return self.element[int(value)-1].identify()
+			indexed = self.element[int(value)-1]
 		except:
 			try:
 				for element in self.element:
-					if float(element.weight) == float(value):
-						return element.identify()
+					if float(element.getWeight()) == float(value):
+						indexed = element
 			except:
 				pass
 		if len(value)<=2:
 			for element in self.element:
-				if element.symbol == value:
-					return element.identify()
+				if element.getSymbol() == value:
+					indexed = element
 		else:
 			for element in self.element:
-				if element.name == value:
-					return element.identify()
+				if element.getName() == value:
+					indexed = element
+		return "Element Name: " + indexed.getName() + "\nElement Symbol: " + indexed.getSymbol() + "\nAtomic Number: " + str(indexed.getNumber()) + "\nAtomic Weight: " + str(indexed.getWeight())
 	def parse(self,compound):
 		compound = list(compound) # https://stackoverflow.com/questions/4978787/how-to-split-a-string-into-array-of-characters
 		totalweight = 0.0
@@ -94,11 +117,13 @@ class PeriodicTable:
 					else:
 						if compound[n+1].isalpha()==False:
 							multiplier = float(compound[n+1])
+							if compound[n+2].isalpha()==False:
+								multiplier = float(str(compound[n+1])+str(compound[n+2]))
 				except IndexError:
 					pass
 				for element in self.element:
-					if element.symbol == symbol:
-						totalweight += float(element.weight)*multiplier
+					if element.getSymbol() == symbol:
+						totalweight += float(element.getWeight())*multiplier
 		return totalweight
 	def __str__(self):
 		return str(self.element)
@@ -112,7 +137,10 @@ def identify(response):
 		if responselist[n].isupper()==True:
 			elementcount+=1
 		elif responselist[n].isalpha()==False:
-			number+=1
+			if responselist[n]==" ":
+				sys.exit()
+			else:
+				number+=1
 	if (number>=1 and elementcount>=1) or (elementcount>1):
 		return initPeriodicTable.parse(response)
 	else:
